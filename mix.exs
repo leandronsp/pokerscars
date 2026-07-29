14686bf5af2404661226b3d7f6e0226363ee0559
Leandro Proença <leandronsp@gmail.com>
2026-07-29 10:12:17 -0300

    chore: add stream_data for property tests

--- mix.exs
+++ mix.exs
@@ -74,6 +74,7 @@ defmodule Pokerscars.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
+      {:stream_data, "~> 1.0", only: [:dev, :test]},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
